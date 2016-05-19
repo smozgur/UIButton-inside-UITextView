f94af150ccd8bab0b5b8e977673c976eafead72b
Suat Özgür <suat@smozgur.com>
2016-05-19 19:36:54 +0300

    set uibutton font to be  textview font. trying to figure out uibutton insets for best result.

--- Button In TextView/ViewController.swift
+++ Button In TextView/ViewController.swift
@@ -17,19 +17,21 @@ class ViewController: UIViewController {
         
         let button   = UIButton(type: UIButtonType.System) as UIButton
         
+        button.titleLabel?.font = txtView.font
         button.frame = CGRectMake(0, 0, 0, 0)
-        button.contentEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        button.contentEdgeInsets = UIEdgeInsets(top: 9, left: 5, bottom: 0, right: 0)
+        button.titleEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        
         button.setTitle("itsthefinalcountdown", forState: UIControlState.Normal)
-        button.backgroundColor = UIColor.yellowColor()
+        // button.backgroundColor = UIColor.yellowColor()
         button.sizeToFit()
         
         
-        let buttonFrame = button.frame;
+        let buttonFrame = CGRect(x: 0, y: 0, width: button.frame.size.width, height: (txtView.font?.lineHeight)! as CGFloat)
         let exclusivePath = UIBezierPath(rect: buttonFrame)
         txtView.textContainer.exclusionPaths = [exclusivePath]
         
         txtView.addSubview(button)
-        
         
     }
     
