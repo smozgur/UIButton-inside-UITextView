6f2e4359f26fce3675682f3d46d99f9784f57b6b
Suat Özgür <suat@smozgur.com>
2016-05-19 18:17:03 +0300

    Initial

--- Button In TextView/ViewController.swift
+++ Button In TextView/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  Button In TextView
+//  Test
 //
 //  Created by Suat Ozgur on 5/19/16.
 //  Copyright © 2016 BatCoder. All rights reserved.
@@ -9,17 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var txtView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let button   = UIButton(type: UIButtonType.System) as UIButton
+        
+        button.frame = CGRectMake(0, 0, 0, 0)
+        button.contentEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        button.setTitle("itsthefinalcountdown", forState: UIControlState.Normal)
+        button.backgroundColor = UIColor.yellowColor()
+        button.sizeToFit()
+        
+        
+        let buttonFrame = button.frame;
+        let exclusivePath = UIBezierPath(rect: buttonFrame)
+        txtView.textContainer.exclusionPaths = [exclusivePath]
+        
+        txtView.addSubview(button)
+        
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
